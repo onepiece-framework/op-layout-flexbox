8def63c89ccfe6177c2ae625f4d9ccf323a92c67
Tomoaki macbook12i7 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-10-11 19:29:03 +0900

    DOC: PHPDoc

--- css/action.php
+++ css/action.php
@@ -1,10 +1,9 @@
 <?php
-/**
- * layout-white:/css/action.php
+/** op-layout-white:/css/action.php
  *
  * @created   2017-09-06
  * @version   1.0
- * @package   layout-white
+ * @package   op-layout-white
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
