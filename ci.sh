bf6416b31f53e2319d27bcd606fd5d1b22d15948
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-11-14 18:38:45 +0900

    New: ci.sh

---
+++ ci.sh
@@ -0,0 +1,12 @@
+
+ ## op-layout-white:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created   2022-11-14
+ # @version   1.0
+ # @package   op-layout-white
+ # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright Tomoaki Nagahara All right reserved.
+
+php ../../../ci.php display=0
