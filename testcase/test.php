1d73d7dc7a41867ab6f44b0fc46004ea2bf77609
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-05 16:01:28 +0900

    New: testcase > test

---
+++ testcase/test.php
@@ -0,0 +1,17 @@
+<?php
+/** op-layout-white:/testcase/test.php
+ *
+ * @created   2023-02-03
+ * @version   1.0
+ * @package   op-layout-white
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+OP::Html('This is layout of white.');
