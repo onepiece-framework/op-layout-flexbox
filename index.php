bf578e36fde0fa82ef39a2372d29776c1d0baa5a
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-05-11 12:18:53 +0900

    Fix: op-unit-bulgaria --> op-unit-flexbox

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-/** op-layout-white:/index.php
+/** op-layout-flexbox:/index.php
  *
- * @created   2016-11-22
- * @version   1.0
- * @package   op-layout-white
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright Tomoaki Nagahara All right reserved.
+ * @created     2023-05-11
+ * @version     1.0
+ * @package     op-layout-flexbox
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
  */
 include('html.phtml');
