442b06fe8806f7276706e1b549aec7d113d679c1
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-06 22:02:04 +0900

    New: config.php | Default config.

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+/** op-layout-flexbox:/config.php
+ *
+ * @created     2023-02-06
+ * @version     1.0
+ * @package     op-layout-flexbox
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\LAYOUT\FLEXBOX;
+
+//	...
+if( OP()->Env()->isAdmin() ){
+	//	In case of set debug flag.
+	if( ($debug = OP()->Request('debug')['flexbox'] ?? null) !== null ){
+		//	Save to session.
+		\OP\Session::Set('debug', $debug);
+	}
+	//	For admin.
+	$debug = \OP\Session::Get('debug');
+}else{
+	//	Not admin user.
+	$debug = false;
+}
+
+//	...
+return [
+	'debug' => $debug,
+];
