f12a9a3072cf77db7b15e46a8b1e09b53aab3419
MacStudioUltra128GB <TomoakiNagahara@github.com>
2024-10-17 03:19:38 +0900

    Fix: js > console.js | Timestamp is display to console.

--- js/console.js
+++ js/console.js
@@ -1,2 +1,11 @@
 
-console.log('flexbox!!');
+/** op-layout-flexbox:/js/console.js
+ *
+ * @created     2024-10-17
+ * @version     1.0
+ * @package     op-layout-flexbox
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+console.log('<?= OP()->Env()->Timestamp()  ?>');
